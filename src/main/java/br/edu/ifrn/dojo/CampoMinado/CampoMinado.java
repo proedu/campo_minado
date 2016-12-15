c92b227a32ce4f39d9ef40e5660fbf72f6b32259
Leonardo Minora <leonardo.minora@gmail.com>
2016-12-15 15:12:45 -0300

    Adicionado CampoMinado.java com método de acesso aos valores do campo
    Changes to be committed:
    	new file:   src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java

---
+++ src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
@@ -0,0 +1,12 @@
+package br.edu.ifrn.dojo.CampoMinado;
+
+public class CampoMinado {
+
+    private void atribui(int linha, int coluna, int valor) {
+
+    }
+
+    private int pegue(int linha, int coluna) {
+        return -1;
+    }
+}
