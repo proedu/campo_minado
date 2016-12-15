7250a6a6a315a81a92f552356fa7b1ab76195e07
brunogomes3000 <brunogomes3000@gmail.com>
2016-12-15 15:55:45 -0300

    Adicionado o método de inserir minas no campo minado

--- src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
+++ src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
@@ -1,6 +1,10 @@
 package br.edu.ifrn.dojo.CampoMinado;
 
+import java.util.Random;
+
 public class CampoMinado {
+
+   private Random r1;	
 
     private void atribui(int linha, int coluna, int valor) {
 
@@ -10,3 +14,24 @@ public class CampoMinado {
         return -1;
     }
 }
+
+    private void posicionarMinas( ){
+
+	r1 = new Random();
+	qtd =0;
+	int linha, coluna, qtd=0;
+
+	while((qtd<valor)){
+		linha = r1.nextInt(campo[][].length);
+		coluna = r1.nextInt(campo[].length);
+
+		if(campo[linha][coluna]!=null){
+			continue;
+		}else{
+			campo[linha][coluna] = -1;
+			qtd++;
+		}
+	}
+     }
+
+   }
