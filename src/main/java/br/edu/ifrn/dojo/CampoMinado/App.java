c46f25ab6d3ddd6ab26762dc8e8f811077476bfb
root <root@debian.ifrn.local>
2016-12-15 15:42:13 -0300

    alteraçao app.java

--- src/main/java/br/edu/ifrn/dojo/CampoMinado/App.java
+++ src/main/java/br/edu/ifrn/dojo/CampoMinado/App.java
@@ -4,12 +4,13 @@ package br.edu.ifrn.dojo.CampoMinado;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
 	System.out.println( "Bruno, Vitor e Victor");
 	System.out.println( "Hello World! Pablo e Aecio" );
+  System.out.println("Hello Word! Joel");
     }
 }
