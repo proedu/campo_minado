5df2e5595daa4a12a7154cd4ee735146d352eed3
root <root@debian.ifrn.local>
2016-12-15 16:02:25 -0300

    Criei o mapa do campo

--- src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
+++ src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
@@ -2,6 +2,15 @@ package br.edu.ifrn.dojo.CampoMinado;
 
 public class CampoMinado {
 
+    private int[][] valores;
+    private int[][] estados;
+
+
+    public CampoMinado(int linhaEColuna, int qtdBombas) {
+      this.valores = new int[linhaEColuna][linhaEColuna]();
+      this.estados = new int[linhaEColuna][linhaEColuna]();
+    }
+
     private void atribui(int linha, int coluna, int valor) {
 
     }
