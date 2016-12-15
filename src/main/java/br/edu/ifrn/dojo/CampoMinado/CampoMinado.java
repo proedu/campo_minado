f6bc6be6273c3d04de1a37e0aa50e42bec7aa5ab
Adrianny <adriannyingryd@gmail.com>
2016-12-15 16:19:28 -0300

    Implementação do método minasAtivas

--- src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
+++ src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
@@ -2,6 +2,9 @@ package br.edu.ifrn.dojo.CampoMinado;
 
 public class CampoMinado {
 
+    private int[][] campo;
+    private Boolean valor = true;
+
     private void atribui(int linha, int coluna, int valor) {
 
     }
@@ -9,4 +12,20 @@ public class CampoMinado {
     private int pegue(int linha, int coluna) {
         return -1;
     }
+
+    private int minasAtivas(int[][] campoM){
+      int qtdeMinas = 0;
+
+      for(int i = 0; i< campoM.length; i++){
+        for(int j = 0; j < campoM.length; j++){
+          if(campoM[i][j] == -1 && valor==false){
+            qtdeMinas++;
+          }
+          j++;
+        }
+        i++;
+      }
+      return qtdeMinas;
+    }
+
 }
