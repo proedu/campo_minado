1d51ae929e218392e4dde21270ed45df73a2c861
root <root@debian.ifrn.local>
2016-12-15 16:13:58 -0300

    Criei o Mapa do campo

--- src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
+++ src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
@@ -4,45 +4,43 @@ import java.util.Random;
 
 public class CampoMinado {
 
-<<<<<<< HEAD
-   private Random r1;	
-=======
-    private int[][] valores;
-    private int[][] estados;
+
+  private Random r1;
+
+  private int[][] valores;
+  private int[][] estados;
 
 
-    public CampoMinado(int linhaEColuna, int qtdBombas) {
-      this.valores = new int[linhaEColuna][linhaEColuna]();
-      this.estados = new int[linhaEColuna][linhaEColuna]();
-    }
->>>>>>> tarefa-1
+  public CampoMinado(int linhaEColuna, int qtdBombas) {
+    this.valores = new int[linhaEColuna][linhaEColuna]();
+    this.estados = new int[linhaEColuna][linhaEColuna]();
+  }
 
-    private void atribui(int linha, int coluna, int valor) {
 
-    }
+  private void atribui(int linha, int coluna, int valor) {
 
-    private int pegue(int linha, int coluna) {
-        return -1;
-    }
+  }
+
+  private int pegue(int linha, int coluna) {
+    return -1;
+  }
 }
 
-    private void posicionarMinas( ){
+private void posicionarMinas( ){
 
-	r1 = new Random();
-	qtd =0;
-	int linha, coluna, qtd=0;
+  r1 = new Random();
+  qtd =0;
+  int linha, coluna, qtd=0;
 
-	while((qtd<valor)){
-		linha = r1.nextInt(campo[][].length);
-		coluna = r1.nextInt(campo[].length);
+  while((qtd<valor)){
+    linha = r1.nextInt(campo[][].length);
+    coluna = r1.nextInt(campo[].length);
 
-		if(campo[linha][coluna]!=null){
-			continue;
-		}else{
-			campo[linha][coluna] = -1;
-			qtd++;
-		}
-	}
-     }
-
-   }
+    if(campo[linha][coluna]!=null){
+      continue;
+    }else{
+      campo[linha][coluna] = -1;
+      qtd++;
+    }
+  }
+}
