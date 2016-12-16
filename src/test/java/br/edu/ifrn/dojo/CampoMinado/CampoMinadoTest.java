91810dc98b8c50bbd4ed58075f22859e19cd3f6f
Pekos Orrico <pabloppekos@gmail.com>
2016-12-16 15:00:24 -0300

    inicio do trabalho com testes unitários
    Mudanças a serem submetidas:
    	modified:   pom.xml
    	new file:   src/test/java/br/edu/ifrn/dojo/CampoMinado/CampoMinadoTest.java

---
+++ src/test/java/br/edu/ifrn/dojo/CampoMinado/CampoMinadoTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.dojo.CampoMinado;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pekos
+ */
+public class CampoMinadoTest {
+    
+    public CampoMinadoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test  here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {
+        CampoMinado campo = new CampoMinado();
+        assertNotNull(campo);
+    }
+    
+    
+}
