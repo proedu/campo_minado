3486c3ce416f0bd813e070f4b0da00c0ebf38bdc
Pekos Orrico <pabloppekos@gmail.com>
2016-12-16 15:56:51 -0300

    Modificação nos nomes valores e estados

--- src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
+++ src/main/java/br/edu/ifrn/dojo/CampoMinado/CampoMinado.java
@@ -1,16 +1,16 @@
 package br.edu.ifrn.dojo.CampoMinado;
 
 public class CampoMinado {
-    private int[][] valores;
-    private int[][] estados;
+    private int[][] valore;
+    private int[][] estado;
     private int quantidadeBombas;
-    
+
     static CampoMinado criar(int tamanho, int qtqBombas) {
         CampoMinado campo = new CampoMinado();
-        campo.valores = new int[tamanho][tamanho];
-        campo.estados = new int[tamanho][tamanho];
+        campo.valore = new int[tamanho][tamanho];
+        campo.estado = new int[tamanho][tamanho];
         campo.quantidadeBombas = qtqBombas;
-        
+
         return campo;
     }
 
